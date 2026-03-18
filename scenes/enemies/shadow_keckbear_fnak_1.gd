63cfebb8c030d66c8e020f48d4182c6fe8c04ee4
olinolinolinolin <141559185+olinolinolinolin@users.noreply.github.com>
2026-03-18 16:14:23 -0500

    added "shadow" keckbear and cleaned it uppp

---
+++ scenes/enemies/shadow_keckbear_fnak_1.gd
@@ -0,0 +1,52 @@
+extends Node3D
+var lightbool = false
+var agitation = 0.0
+var scared = 1.0
+var canbeseen = false
+var rng = RandomNumberGenerator.new()
+@export var spawnlocations : PackedVector3Array
+@export_category("attacktimes")
+@export var attacktime1: float
+@export var attacktime2: float
+signal ShadowKills
+var aledead = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	determinetimetoscare()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if canbeseen == true and lightbool == false:
+		agitation += .1 * delta
+	if agitation >= 1:
+		killale()
+	if canbeseen == true and lightbool == true:
+		scared -= .25 * delta
+		if scared <= 0:
+			canbeseen = false
+			$KeckbearSprite.hide()
+			agitation = 0
+			determinetimetoscare()
+
+func _on_node_3d_lightonshadow(flashlight) -> void:
+	lightbool = flashlight
+
+func determinetimetoscare():
+	$SpawnTimer.wait_time = rng.randi_range(attacktime1,attacktime2)
+	$SpawnTimer.start()
+
+func _on_spawn_timer_timeout() -> void:
+	position = spawnlocations[rng.randi_range(0,2)]
+	$KeckbearSprite.show()
+	$ShadowSFX.play()
+	canbeseen = true
+
+
+func killale():
+	if aledead == false:
+		ShadowKills.emit()
+		print("shadow kills")
+		aledead = true
--- testhub_1.gd
+++ testhub_1.gd
@@ -1,5 +1,8 @@
 extends Node3D
 
+var KeckbearFlashed = false
+signal lightonshadow
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +12,17 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_flash_light_area_body_entered(body: Node3D) -> void:
+	if body.is_in_group("Shadow") == true:
+		KeckbearFlashed = true
+		print("flashing")
+		lightonshadow.emit(KeckbearFlashed)
+
+
+func _on_flash_light_area_body_exited(body: Node3D) -> void:
+	if body.is_in_group("Shadow") == true:
+		KeckbearFlashed = false
+		print("no flashing")
+		lightonshadow.emit(KeckbearFlashed)
